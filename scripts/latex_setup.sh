f4e9c2d9419ee167510d68aeb1ed4954c568a787
Neil Bardhan <neilbardhan@Neils-MacBook-Pro.local>
2025-10-03 15:36:37 -0700

    added latex compile in latex_setup.sh

--- scripts/latex_setup.sh
+++ scripts/latex_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+pwd && ls -lah /app/
 apt-get update \
+&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
 && apt install -y wget \
 && apt-get install -y perl-doc \
-&& cpan -y Pod::Usage \
+&& cpan --yes Pod::Usage
 
 # apt remove 'texlive*'
 # apt-get install -y texlive-latex-base #texlive-extra-utils
@@ -12,15 +14,17 @@ wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz  \
 && zcat < install-tl-unx.tar.gz | tar xf - \
 && cd install-tl-2* \
 && perl ./install-tl --no-interaction --scheme=small --no-doc-install --no-src-install \
-&& export PATH="/usr/local/texlive/2025/bin/x86_64-linux:$PATH"
+&& export PATH="/usr/local/texlive/2025/bin/x86_64-linux:$PATH" \
+&& cd ..
 
 # apt-get install -y xzdec
-tlmgr init-usertree
 # tlmgr repository list
 # tlmgr repository add ftp://tug.org/historic/systems/texlive/2023/tlnet-final
 # # tlmgr repository remove http://mirror.ctan.org/systems/texlive/tlnet
 # tlmgr option repository ftp://tug.org/historic/systems/texlive/2023/tlnet-final
-tlmgr update --all \
+
+tlmgr init-usertree \
+&& tlmgr update --all \
 && tlmgr install fira \
 && tlmgr install fontawesome5 \
 && tlmgr install fontaxes \
@@ -28,4 +32,7 @@ tlmgr update --all \
 && tlmgr install scalerel \
 && tlmgr install stackengine \
 && tlmgr install newtxsf
-# pdflatex --version
+
+cd /app/ \
+&& pdflatex sample.tex \
+&& ls -lah
