e810946734c4d5b9e807cfc98b59950f82ce84e7
Neil Bardhan <neilbardhan@Neils-MacBook-Pro.local>
2025-10-03 00:28:32 -0700

    stashing commits

--- scripts/latex_setup.sh
+++ scripts/latex_setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 apt-get update
-apt remove 'texlive*'
-apt-get install -y texlive-latex-base #texlive-extra-utils
-apt-get install xzdec
-# tlmgr init-usertree
+apt install -y wget
+# apt-get install -y perl-CPAN
+apt-get install -y perl-doc
+cpan -y Pod::Usage
+# apt remove 'texlive*'
+# apt-get install -y texlive-latex-base #texlive-extra-utils
+
+wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz 
+zcat < install-tl-unx.tar.gz | tar xf -
+cd install-tl-2*
+perl ./install-tl --no-interaction --scheme=small --no-doc-install --no-src-install 
+export PATH="/usr/local/texlive/2025/bin/x86_64-linux:$PATH"
+
+# apt-get install -y xzdec
+tlmgr init-usertree
+# tlmgr repository list
 # tlmgr repository add ftp://tug.org/historic/systems/texlive/2023/tlnet-final
 # # tlmgr repository remove http://mirror.ctan.org/systems/texlive/tlnet
 # tlmgr option repository ftp://tug.org/historic/systems/texlive/2023/tlnet-final
-# tlmgr update --all
-# tlmgr --verify-repo=none install fira
+tlmgr update --all
+tlmgr install fira fontawesome5 fontaxes mwe tikz scalerel stackengine newtxsf
 # pdflatex --version
