8a273fed84b64f40abb29bbdc7c3722a9755c02a
Neil Bardhan <neilbardhan@Neils-MacBook-Pro.local>
2025-10-03 16:05:53 -0700

    Improved docker setup and latex compilation steps

--- scripts/latex_setup.sh
+++ scripts/latex_setup.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
+set -e
 
-pwd && ls -lah /app/
-apt-get update \
-&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-&& apt install -y wget \
-&& apt-get install -y perl-doc \
-&& cpan --yes Pod::Usage
+if [ $# -lt 1 ]; then
+    echo "Usage: latex_setup.sh <file.tex>"
+    exit 1
+fi
 
-# apt remove 'texlive*'
-# apt-get install -y texlive-latex-base #texlive-extra-utils
+TEX_FILE="$1"
 
-wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz  \
-&& zcat < install-tl-unx.tar.gz | tar xf - \
-&& cd install-tl-2* \
-&& perl ./install-tl --no-interaction --scheme=small --no-doc-install --no-src-install \
-&& export PATH="/usr/local/texlive/2025/bin/x86_64-linux:$PATH" \
-&& cd ..
+if [ ! -f "$TEX_FILE" ]; then
+    echo "Error: File $TEX_FILE not found."
+    exit 1
+fi
 
-# apt-get install -y xzdec
-# tlmgr repository list
-# tlmgr repository add ftp://tug.org/historic/systems/texlive/2023/tlnet-final
-# # tlmgr repository remove http://mirror.ctan.org/systems/texlive/tlnet
-# tlmgr option repository ftp://tug.org/historic/systems/texlive/2023/tlnet-final
+TEX_DIR=$(dirname "$TEX_FILE")
+BASENAME=$(basename "$TEX_FILE")
 
-tlmgr init-usertree \
-&& tlmgr update --all \
-&& tlmgr install fira \
-&& tlmgr install fontawesome5 \
-&& tlmgr install fontaxes \
-&& tlmgr install mwe \
-&& tlmgr install scalerel \
-&& tlmgr install stackengine \
-&& tlmgr install newtxsf
+echo "Compiling $TEX_FILE..."
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory="$TEX_DIR" "$TEX_FILE"
 
-cd /app/ \
-&& pdflatex sample.tex \
-&& ls -lah
+echo "Done. PDF created at $TEX_DIR/${BASENAME%.tex}.pdf"
